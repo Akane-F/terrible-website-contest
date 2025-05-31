24966adf5f72c3fc6a1060e9f5ff79e39d80567e
Akane-F <akanefukuda.26@gmail.com>
2025-05-30 18:46:07 -0700

    Add moving Effect to Biglogo

--- js/script.js
+++ js/script.js
@@ -60,3 +60,46 @@ for (let i = 0; i < 100; i++) {
 
     grid3.appendChild(tile);
 }
+
+window.onload = () => {
+    const img = document.getElementById('big-logo-image');
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    // Initialize
+    let posX = (screenWidth - img.offsetWidth) / 2;
+    let posY = (screenHeight - img.offsetHeight) / 2;
+    let velX = 3;
+    let velY = 2;
+
+    const imgWidth = img.offsetWidth;
+
+    function animate() {
+      // Moving
+      posX += velX;
+      posY += velY;
+
+      // Reflect
+      if (posX >= screenWidth - imgWidth || posX <= 0) {
+        velX *= -1;
+        posX = Math.max(0, Math.min(posX, screenWidth - imgWidth));
+      }
+      if (posY >= screenHeight - imgWidth|| posY <= 0) {
+        velY *= -1;
+        posY = Math.max(0, Math.min(posY, screenHeight - imgWidth));
+      }
+
+      // Update drawing
+      img.style.left = posX + 'px';
+      img.style.top = posY + 'px';
+
+      requestAnimationFrame(animate);
+    }
+
+    img.style.left = posX + 'px';
+    img.style.top = posY + 'px';
+
+    setTimeout(() => {
+        animate();
+    }, 500)
+}
