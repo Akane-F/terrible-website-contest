ce7fa532c78c775b2b7c8e770d8a3ee42a84881c
Akane-F <akanefukuda.26@gmail.com>
2025-05-30 19:49:41 -0700

    Change Drawing Grid on click

--- js/script.js
+++ js/script.js
@@ -1,64 +1,28 @@
-const grid = document.getElementById("grid");
-const grid2 = document.getElementById("grid2");
-const grid3 = document.getElementById("grid3");
-const sizes = ["size-1x1", "size-2x2", "size-3x3", "size-4x4"];
-const laughShakes = ["laughShake1", "laughShake2"];
+function drawWaraiotoko(gridName) {
+    const grid = document.getElementById(gridName);
+    const sizes = ["size-1x1", "size-2x2", "size-3x3", "size-4x4"];
+    const laughShakes = ["laughShake1", "laughShake2"];
+    let i = 0;
 
-for (let i = 0; i < 100; i++) {
-    const tile = document.createElement("div");
-    const size = sizes[Math.floor(Math.random() * sizes.length)];
-    const laughShake = laughShakes[Math.floor(Math.random() * laughShakes.length)];
-    tile.className = `tile ${size} ${laughShake}`;
+    const drawTileInterval = setInterval(() => {
+        const tile = document.createElement("div");
+        const size = sizes[Math.floor(Math.random() * sizes.length)];
+        const laughShake = laughShakes[Math.floor(Math.random() * laughShakes.length)];
+        tile.className = `tile ${size} ${laughShake}`;
+    
+        const img = document.createElement("img");
+        img.src = "./images/waraiotoko.png";
+        img.alt = "Warai Otoko";
+    
+        tile.appendChild(img);
+        grid.appendChild(tile);   
 
-    const img = document.createElement("img");
-    img.src = "./images/waraiotoko.png";
-    tile.appendChild(img);
-
-    grid.appendChild(tile);
-}
-
-for (let i = 0; i < 100; i++) {
-    const tile = document.createElement("div");
-    const size = sizes[Math.floor(Math.random() * sizes.length)];
-    const laughShake = laughShakes[Math.floor(Math.random() * laughShakes.length)];
-    tile.className = `tile ${size} ${laughShake}`;
-
-    const img = document.createElement("img");
-    img.src = "./images/waraiotoko.png";
-    tile.appendChild(img);
-
-    grid2.appendChild(tile);
-}
-
-for (let i = 0; i < 100; i++) {
-    const tile = document.createElement("div");
-    const size = sizes[Math.floor(Math.random() * sizes.length)];
-    const laughShake = laughShakes[Math.floor(Math.random() * laughShakes.length)];
-    tile.className = `tile ${size} ${laughShake} laughImage`;
-
-    const img = document.createElement("img");
-    img.src = "./images/waraiotoko.png";
-
-    img.addEventListener('click', () => {
-        const laughSoundSrc = "./videos/chipmunks-hehehei-113723.mp3";
-        const laughSound = new Audio(laughSoundSrc);
-        laughSound.loop = true;
-        laughSound.play();
-
-        img.classList.add('fade-out');
-        setTimeout(() => {
-            img.style.display = 'none';
-        }, 500);
-
-        setTimeout(() => {
-            img.style.display = 'block';
-            img.classList.remove('fade-out');
-        }, 1000);
-    });
-
-    tile.appendChild(img);
-
-    grid3.appendChild(tile);
+        if (i > 100){
+            clearInterval(drawTileInterval);
+        } else {
+            i++;
+        }
+    }, 50)
 }
 
 window.onload = () => {
@@ -69,6 +33,10 @@ window.onload = () => {
         const laughSound = new Audio(laughSoundSrc);
         laughSound.loop = true;
         laughSound.play();
+
+        drawWaraiotoko("grid1");
+        drawWaraiotoko("grid2");
+        drawWaraiotoko("grid3");
     });
 
     const screenWidth = window.innerWidth;
